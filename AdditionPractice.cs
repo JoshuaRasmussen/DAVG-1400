125703e7b62c6f7507a5eb4e3b0470deb0e11c1a
JoshuaRasmussen <158103082+JoshuaRasmussen@users.noreply.github.com>
2024-05-15 15:40:50 -0600

    Create AdditionPractice.cs

---
+++ AdditionPractice.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 213 + 74;
+      int y = 124 + 510;
+      Console.WriteLine(x);  
+      Console.WriteLine(y);
+     }
+  }
+}
