cac238e3492847c2f4ff767a5963eb297cf89cbf
Joshua Rasmussen <10859822@uvu.edu>
2024-07-20 15:42:10 -0600

    Submission for Lab 12 lab

---
+++ UFO Defense Force/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float xRange;
+    public Transform blaster;
+    public GameObject Projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set HorizontalInput to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves player left and right
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        // Keeps player within bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // if spacebar is pressed, sends out projectile
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create Projectile at the blaster transform position maintaining the objects rotation.
+            Instantiate(Projectile, blaster.transform.position, Projectile.transform.rotation);
+        }
+
+        
+    }
+    
+    // deletes objects with trigger that hits the player
+    private void  OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
